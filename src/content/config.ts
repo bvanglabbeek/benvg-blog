2ca9b37135fe714dde1fce52538f814afd82e0dc
Ben VG <ben@benvg.com>
2025-04-22 22:46:40 -0400

    lots of clean up 2

--- src/content/config.ts
+++ src/content/config.ts
@@ -4,13 +4,13 @@ const blogCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    pubDate: z.string().transform(str => new Date(str)),
+    pubDate: z.coerce.date(),
     author: z.string(),
     tags: z.array(z.string()),
-    image: z.string().optional()
-  })
+    image: z.string().optional(),
+  }),
 });
 
 export const collections = {
-  blog: blogCollection
+  blog: blogCollection,
 };
