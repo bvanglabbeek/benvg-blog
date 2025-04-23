8185de62b5acb4ad369eb200b919cb9f780e0595
Ben VG <ben@benvg.com>
2025-04-22 22:39:05 -0400

    lots of clean up

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,16 +1,16 @@
 import { defineCollection, z } from 'astro:content';
 
-const blog = defineCollection({
+const blogCollection = defineCollection({
   schema: z.object({
     title: z.string(),
     description: z.string(),
-    pubDate: z.date(),
+    pubDate: z.string().transform(str => new Date(str)),
     author: z.string(),
     tags: z.array(z.string()),
-    image: z.string().optional(),
-  }),
+    image: z.string().optional()
+  })
 });
 
 export const collections = {
-  blog,
+  blog: blogCollection
 };
