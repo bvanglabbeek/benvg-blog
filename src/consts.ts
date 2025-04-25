825a45552f9965ea49efadbeff938e715e3a4188
Ben VG <ben@benvg.com>
2025-04-25 12:13:07 -0400

    clean up the code

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,36 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = "Astro Blog";
-export const SITE_DESCRIPTION = "Welcome to my website!";
+// Site metadata
+export const SITE_TITLE = "Ben van Glabbeek";
+export const SITE_DESCRIPTION = "Personal blog and portfolio of Ben van Glabbeek - Transformation, Software Engineering, adventurer, and lifelong learner.";
+export const SITE_URL = "https://benvglabbeek.com";
+
+// Social media
+export const LINKEDIN_HANDLE = "bvanglabbeek";
+
+// Navigation
+export const NAV_ITEMS = [
+  { title: "Home", path: "/" },
+  { title: "Appalachian Trail", path: "/appalachian-trail" },
+  { title: "Transcen.Digital", path: "/transcen-digital" },
+  { title: "About", path: "/about" }
+];
+
+// Social links
+export const SOCIAL_LINKS = {
+  linkedin: "https://linkedin.com/in/bvanglabbeek"
+};
+
+// Blog settings
+export const POSTS_PER_PAGE = 10;
+export const RECENT_POSTS_COUNT = 3;
+export const DEFAULT_POST_IMAGE = "/images/default-post.jpg";
+
+// Date formatting
+export const DATE_FORMAT = "MMMM D, YYYY";
+
+// SEO
+export const DEFAULT_OG_IMAGE = "/images/og-image.jpg";
+export const SITE_LANGUAGE = "en";
+export const SITE_LOCALE = "en_US";
